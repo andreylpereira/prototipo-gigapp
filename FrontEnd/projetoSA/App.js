cb1044546f5becc570690ede85bd37a6907b5736
Andrey Luiz Pereira <andreylpereira@gmail.com>
2021-04-23 19:49:10 -0300

    projeto criado no front e rodando

---
+++ FrontEnd/projetoSA/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" hidden={true} />
+      <View>
+        <Text>Testando123</Text>
+      </View>
+    </>
+  );
+};
+
+// const css = StyleSheet.create({
+//   container: {
+//     marginLeft: 130,
+//     color: '#F5D100',
+//   },
+// });
+
+export default App;
