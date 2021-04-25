dd4aed51cc5d40c6d1bf9423a516b4918b3f1a13
Andrey Luiz Pereira <andreylpereira@gmail.com>
2021-04-24 23:53:59 -0300

    correção do componente

---
+++ FrontEnd/projetoSA/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import {StatusBar, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import Home from './components/Home';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" hidden={true} />
+      <View>
+        <TouchableOpacity style={css.button} onPress={() => navigation.navigate('Home')}>
+        <Text style={css.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
+};
+
+
+const css = StyleSheet.create({
+//   container: {
+//     marginLeft: 130,
+//     color: '#F5D100',
+//   },
+      button: {
+        borderWidth: 2,
+        borderRadius: 5,
+        borderColor: 'blue',
+        width: 100,
+        height: 40,
+        backgroundColor: 'dodgerblue',
+        alignSelf: 'center',
+        marginTop: '80%'
+      },
+      buttonText: {
+        textAlign: 'center',
+        color: 'white',
+        marginTop: 6,
+        fontSize: 15,
+        fontWeight: 'bold'
+      }
+});
+
+export default App;
