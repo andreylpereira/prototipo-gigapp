16f6178f3abc7a8f337503d9cce82307e7985177
Andrey Luiz Pereira <andreylpereira@gmail.com>
2021-04-27 17:38:35 -0300

    custimização drawer

--- FrontEnd/projetoSA/src/Router.js
+++ FrontEnd/projetoSA/src/Router.js
@@ -4,7 +4,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import Icon from 'react-native-vector-icons/Ionicons'
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import {
   StatusBar,
@@ -22,6 +22,7 @@ import Sobre from './pages/Sobre';
 import Perfil from './pages/Perfil';
 import Negocio from './pages/Negocio';
 import Opcoes from './pages/Opcoes';
+import { color } from 'react-native-reanimated';
 
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
@@ -31,37 +32,43 @@ const Routes = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName={Login}>
-        <Stack.Screen name="Login" component={Login} 
-        options={{
-          headerShown: false
-        }}
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false,
+          }}
         />
         {/* <Stack.Screen name="Teste" component={Tabs} /> */}
         <Stack.Screen
           name="Home"
           component={Drawers}
-          options={{headerLeft: () => (
-            <View>
-              <Icon
-                name={'musical-notes'}
-                size={37}
-                color={'white'}
-                style={css.container}
-              />
-            </View>
-          ), title: 'Nome do App', headerTitleAlign: 'center',headerTintColor: 'white',
-          headerStyle: {
-            backgroundColor: 'tomato',
-            borderBottomWidth: 1,
-            borderBottomColor: 'red',
-          },
-          
-        }}
+          options={{
+            headerLeft: () => (
+              <View>
+                <Icon
+                  name={'musical-notes'}
+                  size={37}
+                  color={'white'}
+                  style={css.container}
+                />
+              </View>
+            ),
+            title: 'Nome do App',
+            headerTitleAlign: 'center',
+            headerTintColor: 'white',
+            headerStyle: {
+              backgroundColor: 'tomato',
+              borderBottomWidth: 1,
+              borderBottomColor: 'red',
+            },
+          }}
         />
-        <Stack.Screen name="Cadastro"
+        <Stack.Screen
+          name="Cadastro"
           component={Cadastro}
-          options={{headerStyle:{backgroundColor: 'tomato'}, }}
-/>
+          options={{headerStyle: {backgroundColor: 'tomato'}}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -70,75 +77,92 @@ const Routes = () => {
 const Tabs = () => {
   return (
     <Tab.Navigator
-    tabBarOptions={{
-      activeTintColor: 'tomato',
-      inactiveTintColor: 'white',
-      showLabel: false,
-      labelStyle: {
-        fontSize: 15,
-        fontWeight: 'bold',
-        // fontFamily: 'Montserrat-SemiBold',
-      },
-      style: {
-        borderTopWidth: 1,
-        // borderBottomWidth: 2,
-        // borderTopColor: 'red',
-        // borderBottomColor: '#410CF5',
-        backgroundColor: '#131313',
-      },
-    }}
-    >
-      <Tab.Screen name="Home" component={Home} 
-      options={{
-        tabBarLabel: 'HOME',
-        tabBarIcon: ({color, size}) => (
-          <Icon name="home-outline" color={color} size={32} />
-        ),
-      }}/>
-      <Tab.Screen name="Perfil" component={Perfil}  options={{
-        tabBarLabel: 'PERFIL',
-        tabBarIcon: ({color, size}) => (
-          <Icon name="calendar-outline" color={color} size={32} />
-        ),
-      }}/>
-      <Tab.Screen name="Negocio" component={Negocio}  options={{
-        
-        tabBarLabel: 'NEGOCIO',
-        tabBarIcon: ({color, size}) => (
-          <Icon name="list-outline" color={color} size={35} />
-        ),
-      }}/>
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'white',
+        showLabel: false,
+        labelStyle: {
+          fontSize: 15,
+          fontWeight: 'bold',
+          // fontFamily: 'Montserrat-SemiBold',
+        },
+        style: {
+          borderTopWidth: 1,
+          // borderBottomWidth: 2,
+          // borderTopColor: 'red',
+          // borderBottomColor: '#410CF5',
+          backgroundColor: '#131313',
+        },
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'HOME',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="home-outline" color={color} size={32} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Perfil"
+        component={Perfil}
+        options={{
+          tabBarLabel: 'PERFIL',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="calendar-outline" color={color} size={32} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Negocio"
+        component={Negocio}
+        options={{
+          tabBarLabel: 'NEGOCIO',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="list-outline" color={color} size={35} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
 
 const Content = () => {
-  return(
+  return (
     <View style={css.personcontainer}>
-    <View style={css.personLogo}>
-      <Icon name="person-circle" color={'tomato'} size={75} style={css.personLogo} />
+      <View style={css.personLogo}>
+        <Icon
+          name="person-circle"
+          color={'tomato'}
+          size={75}
+          
+        />
+        <View style={css.personDescription}>
+          <Text style={{color: '#ffffff', fontSize: 22, fontWeight:'bold', marginBottom: 2.5}}>Dazaranha</Text>
+          <Text style={{color: '#ffffff', fontSize: 15 , marginBottom: 2.5}}>banda</Text>
+          <Text style={{color: '#ffffff', fontSize: 13, fontStyle:'italic', marginBottom: 10}}>fulano@gmail.com</Text>
+        </View>
+      </View>
     </View>
-    </View>
-  )
-}
+  );
+};
 
 const Drawers = () => {
   return (
     <Drawer.Navigator
-    drawerContent={Content}
-    drawerPosition='left'
-    drawerType='front'
-    drawerStyle={{
-      backgroundColor: '#131313'
+      drawerContent={Content}
+      drawerPosition="left"
+      drawerType="front"
+      drawerStyle={{
+        backgroundColor: '#131313',
       }}
       drawerContentOptions={{
         activeTintColor: 'purple',
         inactiveTintColor: 'red',
         activeBackgroundColor: 'blue',
         inactiveBackgroundColor: 'yellow',
-      }}
-    >
-      
+      }}>
       <Drawer.Screen name="Home" component={Tabs} />
       <Drawer.Screen name="Sobre" component={Sobre} />
       <Drawer.Screen name="Opções" component={Opcoes} />
@@ -147,22 +171,30 @@ const Drawers = () => {
 };
 
 const css = StyleSheet.create({
-    container: {
-      marginLeft: 105,
-      borderWidth: 1,
-      borderRadius: 9.5,
-      backgroundColor: '#131313'
-      // color: '#F5D100',
-    },
-    personContainer: {
-      height: '100%',
-      borderBottomWidth: 1.5,
-      borderBottomColor: '#FFF',
-    },
-    personLogo: {
-      borderBottomWidth: 1,
-      borderBottomColor: '#CDCCCE',
-    },
+  container: {
+    marginLeft: 105,
+    borderWidth: 1,
+    borderRadius: 9.5,
+    backgroundColor: '#131313',
+    // color: '#F5D100',
+  },
+  personContainer: {
+    height: '100%',
+    borderBottomWidth: 1.5,
+    borderBottomColor: '#FFF',
+  },
+  personLogo: {
+    marginTop: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CDCCCE',
+    display: 'flex',
+    flexDirection: 'row',
+    
+  },
+  personDescription: {
+    marginLeft: 5,
+    color: '#fff'
+  }
 });
 
 export default Routes;
