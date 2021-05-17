4c4db9bc123c74b6234657bab3047c2d5410a1db
marzeu <marzahell@hotmail.com>
2021-05-17 20:54:03 -0300

    server e rotas iniciadas

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
 
+import router from './Routes/routes'
+
 const server = express();
 const port = process.env.PORT || 3000;
+
+server.use(express.json());
+server.use('/', router);
 
 server.listen(port, () => { 
     console.log(`Server running in http://localhost:${port}`);
