f5c043b3e3fa79e03c246b7b0bb40d1e172f0177
marzeu <marzahell@hotmail.com>
2021-05-18 22:42:32 -0300

    crud de usuarios

--- BackEnd/src/app/controllers/userController.js
+++ BackEnd/src/app/controllers/userController.js
@@ -10,10 +10,36 @@ class UserController {
         return res.json({ id, name, email, provider });
     }
 
-    async index() { }
-    async show() { }
-    update() { }
-    async delete() { }
+    async update(req, res) {
+        const { email, oldPassword } = req.body;
+        const user = await User.findByPk(req.userId);
+        if (email !== user.email) {
+            const userExists = await User.findOne({
+                where: { email },
+            });
+            if (userExists) {
+                return res.status(400).json({
+                    error: 'User already exists.'
+                });
+            }
+        }
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res.status(401).json({
+                error: 'Password does not match.'
+            });
+        }
+
+        const { id, name, provider } = await user.update(req.body);
+
+        return res.json({ id, name, email, provider });
+    }
+
+    // async index() { }
+    async show(req, res) {
+        const users = await User.findAll();
+        return res.json(users);
+    }
+    // async delete() { }
 
 }
 
--- BackEnd/src/routes.js
+++ BackEnd/src/routes.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 import UserController from './app/controllers/userController';
+import SessionController from './app/controllers/sessionController';
+import AuthMiddleware from './app/middlewares/auth';
+import userController from './app/controllers/userController';
 
 const routes = new Router();
 
@@ -11,10 +14,16 @@ routes.get('/api', (req, res) => {
     });
 });
 
+routes.get('/users', userController.show);
 routes.post('/users', UserController.store);
-routes.post('/users', UserController.index);
-routes.post('/users', UserController.show);
-routes.post('/users', UserController.update);
-routes.post('/users', UserController.delete);
 
-export default router;
+routes.post('/sessions', SessionController.store);
+// routes.post('/users', UserController.index);
+// routes.post('/users', UserController.show);
+// routes.post('/users', UserController.update);
+// routes.post('/users', UserController.delete);
+
+routes.use(AuthMiddleware);
+routes.put('/users', UserController.update);
+
+export default routes;
