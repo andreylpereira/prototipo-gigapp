ce15519bb9dbc2ea7df438067c8347ffd24f25ed
marzeu <marzahell@hotmail.com>
2021-05-19 21:30:10 -0300

    configuração do database no heroku

--- BackEnd/src/config/database.js
+++ BackEnd/src/config/database.js
@@ -1,9 +1,16 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'm12345',
-    database: 'testegig',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    host: 'ec2-52-0-114-209.compute-1.amazonaws.com',
+    username: 'giftmojlglgsbe',
+    password: '1acd9b982bcc1045d39f66ee450b6a9e5c91e02914a98bf14d461b2e40d94b1e',
+    database: 'd2uc06nd8h6o51',
+    ssl: true,
     define: {
         timestamps: true,
         underscored: true,
